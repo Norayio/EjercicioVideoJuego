35c6c3666ebcb76657f832b5c266d28a23b1d4e2
Norayio <pierresediaz@gmail.com>
2024-01-31 13:42:55 -0500

    proyecto creado y clases creadas

---
+++ src/ejerciciovideojuego/EjercicioVideoJuego.java
@@ -0,0 +1,24 @@
+package ejerciciovideojuego;
+
+/**
+ *
+ * @author Norayio
+ */
+
+public class EjercicioVideoJuego {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
