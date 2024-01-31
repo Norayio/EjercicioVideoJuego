385e9d707c18888a4ef1b5a7a6450384056f6735
Norayio <pierresediaz@gmail.com>
2024-01-31 13:55:19 -0500

    listaVideoJuegos creada para guardar los 5 objetos videojuegos creados

--- src/ejerciciovideojuego/EjercicioVideoJuego.java
+++ src/ejerciciovideojuego/EjercicioVideoJuego.java
@@ -1,5 +1,10 @@
 package ejerciciovideojuego;
 
+//imports
+import ejerciciovideojuego.logica.VideoJuego;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Norayio
@@ -9,12 +14,30 @@ public class EjercicioVideoJuego {
 
     public static void main(String[] args) {
         
+        // creamos un ArrayList de tipo VideoJuego
+        List<VideoJuego> listaVideoJuegos = new ArrayList<VideoJuego>( );
         
+        // crear 5 videojuegos y guardarlos en la colección
+        VideoJuego vjuego1 = new VideoJuego( );
         
+        /**
+         *  videoJuego 1
+         */
+        vjuego1.setCodigo(123);
+        vjuego1.setTitulo("Banjo Kazooie");
+        vjuego1.setConsola("Nintendo 64");
+        vjuego1.setCantJugadores(4);
+        vjuego1.setCategoria("plataforma");
         
+         /**
+          * videoJuego 2, 3, 4, 5
+          */
+        VideoJuego vjuego2 = new VideoJuego( 456, "Mario Party 3", "Nintendo 64", 4, "Plataforma");
+        VideoJuego vjuego3 = new VideoJuego( 789, "Age of Empire II", "PC", 1, "Estratefia");
+        VideoJuego vjuego4 = new VideoJuego( 101, "Counter Strike 1.6", "PC", 1, "Shooter");
+        VideoJuego vjuego5 = new VideoJuego( 100, "Mario Kart", "Nintendo 64", 2, "plataforma");
         
-        
-        
+        // agregamos los videojuegos creados a la lista "listaVideoJuegos"
         
         
         
